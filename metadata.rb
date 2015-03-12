2e336fbc223b4cb6cde6fe5fe384ccbfff8a84e2
Ikuo Degawa <degawa@ikuwow.com>
2015-03-12 23:26:38 +0900

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'base'
+maintainer       'ikuwow'
+maintainer_email 'degawa@ikuwow.com'
+license          'All rights reserved'
+description      'Installs/Configures base'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.0'
