cc50b68fbb34eb822536e699a9d1de198c8abfdb
Ikuo Degawa <degawa@ikuwow.com>
2015-03-17 19:43:54 +0900

    degawa userを追加
    .kitchen.ymlでdata_bagのpathを指定できるのね。

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'base'
-depends          'selinux'
+depends          'selinux', '~> 0.9.0'
 maintainer       'ikuwow'
 maintainer_email 'degawa@ikuwow.com'
 license          'All rights reserved'
 description      'Installs/Configures base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -26,3 +26,32 @@ when 'centos', 'redhat'
 end
 
 include_recipe "selinux::disabled"
+
+# degawaユーザーの作成
+user_data = data_bag_item('users','degawa')
+
+user user_data["id"] do
+    supports :manage_home => true
+    home "/home/#{user_data["id"]}"
+    shell "/bin/bash"
+    gid "sudo"
+    action :create
+end
+
+# passwordは手動で設定すること
+
+# ssh鍵設置
+directory "/home/#{user_data["id"]}/.ssh/" do
+    user user_data["id"]
+    group user_data["id"]
+    mode 0700
+    action :create
+end
+
+file "/home/#{user_data["id"]}/.ssh/authorized_keys" do
+    content user_data["public_keys"]
+    user user_data["id"]
+    group user_data["id"]
+    mode 0600
+    action :create
+end
